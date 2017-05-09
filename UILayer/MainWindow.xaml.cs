b083b61d463d3f7832f46365eda7e6a81a237c04
bohdanD <doroshbohdan1@gmail.com>
2017-05-09 12:09:33 +0300

    Add login window

--- UILayer/MainWindow.xaml.cs
+++ UILayer/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DALayer;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UILayer
 {
@@ -20,9 +9,35 @@ namespace UILayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = LoginText.Text;
+            string password = PasswordText.Password;
+
+            try
+            {
+                if (unitOfWork.AdministratorRepository.Login(userName, password))
+                {
+                    //TODO: run another window
+                    MessageBox.Show("success");
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Login or password");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
