d529af11792ddccbb7a090269b16ee11240a4f58
bohdanD <doroshbohdan1@gmail.com>
2017-05-09 16:29:16 +0300

    Add AddOrderViewModel & command

---
+++ UILayer/Commands/AddOrderCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UILayer.Commands
+{
+    public class AddOrderCommand : ICommand
+    {
+        private Func<bool> whenExecute;
+        private Action whatExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public AddOrderCommand(Func<bool> whenExecute, Action whatExecute)
+        {
+            this.whenExecute = whenExecute;
+            this.whatExecute = whatExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return whenExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            whatExecute();
+        }
+    }
+}
---
+++ UILayer/ViewModels/AddOrderViewModel.cs
@@ -0,0 +1,109 @@
+﻿using DALayer;
+using Entities;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using UILayer.Commands;
+
+namespace UILayer.ViewModels
+{   //TODO: add fields validation!!!
+    public class AddOrderViewModel : INotifyPropertyChanged
+    {
+        private UnitOfWork unitOfWork;
+        private Order order;
+        private ICommand addOrderCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AddOrderViewModel()
+        {
+            unitOfWork = new UnitOfWork();
+            order = new Order()
+            {
+                ReceptionDate = DateTime.Now
+            };
+            addOrderCommand = new AddOrderCommand(() => true, AddOrder);
+        }
+
+        private void AddOrder()
+        {
+            try
+            {
+                unitOfWork.OrderRepository.AddOrder(order);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Some thing wrong:\n" + ex.Message);
+            }
+            MessageBox.Show("Succes");
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ICommand AddOrderCommand
+        {
+            get => addOrderCommand;
+        }
+
+        #region UI props
+        public string CustomerName
+        {
+            get => order.CustomerName;
+            set => order.CustomerName = value;
+        }
+
+        public string PhoneNumber
+        {
+            get => order.PhoneNumber;
+            set => order.PhoneNumber = value;
+        }
+
+        public string Email
+        {
+            get => order.Email;
+            set => order.Email = value;
+        }
+
+        public string Device
+        {
+            get => order.Device;
+            set => order.Device = value;
+        }
+
+        public string Problem
+        {
+            get => order.Problem;
+            set => order.Problem = value;
+        }
+
+        public DateTime ReceptionDate
+        {
+            get => order.ReceptionDate;
+            set => order.ReceptionDate = value;
+        }
+
+        public decimal Cost
+        {
+            get => order.Cost;
+            set => order.Cost = value;
+        }
+
+        public decimal Income
+        {
+            get => order.Income;
+            set => order.Income = value;
+        }
+
+        public string Note
+        {
+            get => order.Note;
+            set => order.Note = value;
+        }
+        #endregion
+    }
+}
