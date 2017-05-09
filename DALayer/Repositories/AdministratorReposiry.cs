451a36e7100d39215903e6811c1bec0b25f76638
bohdanD <doroshbohdan1@gmail.com>
2017-05-09 11:09:40 +0300

    change admin repository

--- DALayer/Repositories/AdministratorReposiry.cs
+++ DALayer/Repositories/AdministratorReposiry.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace DALayer.Repositories
 {
-    public class AdministratorReposiry : IAdministratorRepository, IDisposable
+    public class AdministratorReposiry : IAdministratorRepository
     {
         private AccountingContext context;
 
@@ -63,40 +63,5 @@ namespace DALayer.Repositories
             context.Administrator.Add(admin);
             context.SaveChanges();
         }
-
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    context.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~AdministratorReposiry() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
     }
 }
