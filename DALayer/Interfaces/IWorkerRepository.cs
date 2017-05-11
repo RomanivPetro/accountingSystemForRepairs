50c639fbfd1e23aef05c5947a75fadd0c69dac73
bohdanD <doroshbohdan1@gmail.com>
2017-05-11 16:07:26 +0300

    Add work repository

---
+++ DALayer/Interfaces/IWorkerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+
+namespace DALayer.Interfaces
+{
+    public interface IWorkerRepository
+    {
+        void Add(Worker worker);
+
+        void Delete(Worker worker);
+
+        IEnumerable<Worker> GetWorkers();
+
+        int ActiveOrdersCount(Worker worker, DateTime fromDate, DateTime toDate);
+
+        int DoneOrdersCount(Worker worker, DateTime fromDate, DateTime toDate);
+
+        decimal GetDoneOrdersIncome(Worker worker, DateTime fromDate, DateTime toDate);
+    }
+}
---
+++ DALayer/Repositories/WorkerRepository.cs
@@ -0,0 +1,87 @@
+﻿using DALayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using Entities;
+using System.Linq;
+
+namespace DALayer.Repositories
+{
+    public class WorkerRepository : IWorkerRepository
+    {
+        private AccountingContext context;
+
+        public WorkerRepository(AccountingContext context)
+        {
+            this.context = context;
+        }
+
+        private void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private IQueryable<Order> GetOrdersQuery(Worker worker, DateTime fromDate, DateTime toDate)
+        {
+            if (worker == null)
+            {
+                throw new ArgumentNullException("Worker cannot be null");
+            }
+            var query = from w in context.Worker
+                        where w.Id == worker.Id
+                        from o in w.Order
+                        where fromDate <= o.ReceptionDate
+                        && o.ReceptionDate <= toDate
+                        select o;
+            return query;
+        }
+
+        //not tested!!!
+        public int ActiveOrdersCount(Worker worker, DateTime fromDate, DateTime toDate)
+        {
+            var query = GetOrdersQuery(worker, fromDate, toDate)
+                .Where(o => o.GivingDate == null);
+            return query.Count();
+        }
+
+        public void Add(Worker worker)
+        {
+            if (worker == null)
+            {
+                throw new ArgumentNullException("Worker cannot be null");
+            }
+            context.Worker.Add(worker);
+            Save();
+        }
+
+        public void Delete(Worker worker)
+        {
+            if(worker == null)
+            {
+                throw new ArgumentNullException("Worker cannot be null");
+            }
+            context.Worker.Remove(worker);
+            Save();
+        }
+
+        public int DoneOrdersCount(Worker worker, DateTime fromDate, DateTime toDate)
+        {
+            var query = GetOrdersQuery(worker, fromDate, toDate)
+                .Where(o => o.GivingDate != null);
+            return query.Count();
+        }
+
+        public IEnumerable<Worker> GetWorkers()
+        {
+            return context.Worker.ToList();
+        }
+
+        public decimal GetDoneOrdersIncome(Worker worker, DateTime fromDate, DateTime toDate)
+        {
+            var query = GetOrdersQuery(worker, fromDate, toDate)
+                .Where(o => o.GivingDate != null)
+                .Select(o => o.Income)
+                .DefaultIfEmpty(0);
+            return query.Sum();
+        }
+    }
+}
